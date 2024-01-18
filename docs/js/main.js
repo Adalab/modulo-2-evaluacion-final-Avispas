59ee43425d0363008207180c12a26dbadfe3e404
Ángela Ávila Lasanta <lasantavispa@gmail.com>
2024-01-18 20:15:08 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,2 @@
-console.log(">> Ready :)");
+const t=document.querySelector(".js-btnSearch"),n=document.querySelector(".space");function c(){let e=document.getElementById("js-inputText").value;n.innerHTML=e}t.addEventListener("click",c);
 //# sourceMappingURL=main.js.map
